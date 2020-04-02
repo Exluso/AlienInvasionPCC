4042ba14116302fb45ee11ed790b72acd48546e4
Exluso <pg.miceli@gmail.com>
2020-04-02 18:07:36 +0200

    Storing bullets in a group

---
+++ ReadMe.txt
@@ -0,0 +1,2 @@
+The 1st project from Python Crash Course di E. Matthes
+Probably I'll change the cosmetics to something's more Fantasy.
---
+++ alien_invasion.py
@@ -0,0 +1,81 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behaviour."""
+
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # watch for keyboard and mouse events
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_w:
+            self.change_view()
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        """Update images on the screen and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
+
+    def change_view(self):
+        """ Switches between full screen and windowed mode"""
+        if not self.settings.full_screen:
+            self.settings.full_screen = True
+            self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+            self.settings.screen_width = self.screen.get_rect().width
+            self.settings.screen_height = self.screen.get_rect().height
+        elif self.settings.full_screen:
+            self.settings.full_screen = False
+            self.settings.screen_width = 1200
+            self.settings.screen_height = 800
+            self.screen = pygame.display.set_mode(
+                (self.settings.screen_width, self.settings.screen_height))
+
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    # make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
